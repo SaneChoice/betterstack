caf22ddb00e799eca9707d3265a2c730324d3b65
Sohel Rana <sohel@eskimi.com>
2023-06-04 22:14:36 +0600

    Dynamic config from admin and showing monitors using shortcode is done

---
+++ betterstack.php
@@ -0,0 +1,341 @@
+<?php
+/**
+ * @wordpress-plugin
+ * Plugin Name: Better Stack Monitor Status
+ * Description: Display the status of website monitors using the Better Uptime API.
+ * Version: 1.1
+ * Author: SaneChoice Limited
+ * Author URI: https://www.sanechoice.cloud
+ * Requires at least: 6.0
+ * Requires PHP: 7.4
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+register_activation_hook(__FILE__, "wp_php_version_check");
+function wp_php_version_check()
+{
+    // Set minimum required versions
+    $min_wp_version = "6.0";
+    $min_php_version = "7.4";
+
+    // Get current versions
+    global $wp_version;
+    $current_php_version = phpversion();
+
+    // Compare versions
+    if (
+        version_compare($wp_version, $min_wp_version, "<") ||
+        version_compare($current_php_version, $min_php_version, "<")
+    ) {
+        // Deactivate the plugin
+        deactivate_plugins(plugin_basename(__FILE__));
+
+        // Show an error message
+        wp_die(
+            sprintf(
+                __(
+                    'This plugin requires at least WordPress %1$s and PHP %2$s. You are running WordPress %3$s and PHP %4$s. Please upgrade and try again.',
+                    "wp-php-version-check"
+                ),
+                $min_wp_version,
+                $min_php_version,
+                $wp_version,
+                $current_php_version
+            )
+        );
+    }
+}
+
+register_activation_hook(__FILE__, "better_stock_monitor_status_table");
+function better_stock_monitor_status_table()
+{
+    global $wpdb;
+    $charset_collate = $wpdb->get_charset_collate();
+    $table_name = $wpdb->prefix . "better_stock_monitor_status_credentials";
+    $sql = "CREATE TABLE `$table_name` (
+   `id` int(11) NOT NULL AUTO_INCREMENT,
+   `pronounceable_names` text DEFAULT '',
+   `api_key` varchar(220) DEFAULT '',
+   PRIMARY KEY(id)
+   ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+   ";
+    if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
+        require_once ABSPATH . "wp-admin/includes/upgrade.php";
+        dbDelta($sql);
+    }
+}
+
+add_action(
+    "admin_menu",
+    "add_admin_Better_stock_monitor_status_credential_form_page"
+);
+function add_admin_Better_stock_monitor_status_credential_form_page()
+{
+    add_menu_page(
+        "BetterStack Monitor Status Credential",
+        "BetterStack Monitor Status Credential",
+        "manage_options",
+        __FILE__,
+        "crud_admin_better_stock_monitor_status_credential_form_page",
+        "dashicons-wordpress"
+    );
+}
+
+function crud_admin_better_stock_monitor_status_credential_form_page()
+{
+    global $wpdb;
+    $table_name = $wpdb->prefix . "better_stock_monitor_status_credentials";
+    if (isset($_POST["newsubmit"])) {
+        $pronounceable_names = $_POST["pronounceable_names"];
+        $api_key = $_POST["api_key"];
+        $wpdb->query(
+            $wpdb->prepare(
+                "INSERT INTO $table_name(pronounceable_names,api_key) VALUES(%s,%s)", [$pronounceable_names, $api_key]
+            )
+        );
+        echo "<script>location.replace('admin.php?page=betterstack%2Fbetterstack.php');</script>";
+    }
+    if (isset($_POST["uptsubmit"])) {
+        $id = $_POST["uptid"];
+        $pronounceable_names = $_POST["pronounceable_names"];
+        $api_key = $_POST["api_key"];
+        $wpdb->query(
+            $wpdb->prepare(
+                "UPDATE $table_name SET pronounceable_names=%s,api_key=%s WHERE id=%d", [$pronounceable_names, $api_key, $id],
+            )
+        );
+        echo "<script>location.replace('admin.php?page=betterstack%2Fbetterstack.php');</script>";
+    }
+    ?>
+    <div class="wrap">
+        <h2>BetterStack Monitor Status Credential</h2>
+        <table class="wp-list-table widefat striped">
+            <thead>
+            <tr>
+                <th width="60%">Pronounceable Names</th>
+                <th width="25%">API Key</th>
+                <th width="15%">Actions</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            $result = $wpdb->get_results("SELECT * FROM $table_name");
+            if (empty($result)) { ?>
+                <form action="" method="post">
+                    <tr>
+                        <td><input type="text" id="pronounceable_names" name="pronounceable_names"
+                                   placeholder="comma separated pronounceable names eg SaneChoice Website,SaneChoice POP3 etc"
+                                   style='width: 100%;'>
+                        </td>
+                        <td><input type="text" id="api_key" name="api_key"></td>
+                        <td>
+                            <button id="newsubmit" name="newsubmit" type="submit">INSERT</button>
+                        </td>
+                    </tr>
+                </form>
+            <?php }
+            foreach ($result as $print) {
+                echo "
+               <tr>
+                 <td>$print->pronounceable_names</td>
+                 <td>$print->api_key</td>
+                 <td><a href='admin.php?page=betterstack%2Fbetterstack.php&upt=$print->id'><button type='button'>UPDATE</button></a></td>
+               </tr>
+             ";
+            }
+            ?>
+            </tbody>
+        </table>
+        <br>
+        <br>
+        <?php if (isset($_GET["upt"])) {
+            $upt_id = $_GET["upt"];
+            $result = $wpdb->get_results(
+                $wpdb->prepare(
+                    "SELECT * FROM $table_name WHERE id=%d", [$upt_id]
+                )
+            );
+            foreach ($result as $print) {
+                $id = $print->id;
+                $pronounceable_names = $print->pronounceable_names;
+                $api_key = $print->api_key;
+            }
+            if (isset($id) && isset($pronounceable_names) && isset($api_key)) {
+                echo "
+         <table class='wp-list-table widefat striped'>
+           <thead>
+             <tr>
+               <th width='60%'>Pronounceable Names</th>
+               <th width='25%'>API key</th>
+               <th width='15%'>Actions</th>
+             </tr>
+           </thead>
+           <tbody>
+             <form action='' method='post'>
+               <tr>
+                 <td>
+                     <input type='hidden' id='uptid' name='uptid' value='$id'>
+                     <input type='text' id='pronounceable_names' name='pronounceable_names' value='$pronounceable_names' placeholder='comma separated pronounceable names eg SaneChoice Website,SaneChoice POP3 etc' style='width: 100%;'>
+                 </td>
+                 <td><input type='text' id='api_key' name='api_key' value='$api_key'></td>
+                 <td><button id='uptsubmit' name='uptsubmit' type='submit'>UPDATE</button> <a href='admin.php?page=betterstack%2Fbetterstack.php'><button type='button'>CANCEL</button></a></td>
+               </tr>
+             </form>
+           </tbody>
+         </table>";
+            }
+        } ?>
+    </div>
+    <?php
+}
+
+function betteruptime_api_request()
+{
+    global $wpdb;
+    $table_name = $wpdb->prefix . "better_stock_monitor_status_credentials";
+    $result = $wpdb->get_results("SELECT * FROM $table_name limit 1");
+    foreach ($result as $print) {
+        $pronounceable_names = array_map(
+            "trim",
+            explode(",", $print->pronounceable_names)
+        );
+        $api_key = $print->api_key;
+    }
+
+    if (!isset($pronounceable_names) || !isset($api_key)) {
+        return [];
+    }
+
+    $url = "https://betteruptime.com/api/v2/monitors";
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        "Authorization: Bearer $api_key",
+        "Content-Type: application/json",
+    ]);
+
+    $result = curl_exec($ch);
+
+    if (curl_errno($ch)) {
+        return "Error:" . curl_error($ch);
+    }
+
+    curl_close($ch);
+
+    return filterArrayResponse(
+        json_decode($result, true),
+        $pronounceable_names
+    );
+}
+
+function filterArrayResponse($result, $pronounceable_names)
+{
+    $data = [];
+    foreach ($pronounceable_names as $pronounceable_name) {
+        foreach ($result["data"] as $item) {
+            if (
+                $item["attributes"]["pronounceable_name"] == $pronounceable_name
+            ) {
+                $data[] = $item;
+            }
+        }
+    }
+
+    $result["data"] = $data;
+
+    return $result;
+}
+
+function betteruptime_monitor_status_shortcode()
+{
+    $response = betteruptime_api_request();
+
+    if (isset($response["error"])) {
+        return "Error: " . $response["error"]["message"];
+    }
+
+    $output = '<table class="betteruptime">';
+
+    foreach ($response["data"] as $monitor) {
+        $status =
+            $monitor["attributes"]["status"] === "up"
+                ? '<div style="color:MediumSeaGreen;">Operational</div>'
+                : '<div style="color:blue;">Issues Found</div><br><a href="https://status.sanechoice.cloud/">Visit Dashboard</a>';
+        $output .=
+            "<tr><td>" .
+            $monitor["attributes"]["pronounceable_name"] .
+            "</td><td>" .
+            $status .
+            "</td></tr>";
+    }
+
+    $output .= "</table>";
+
+    return $output;
+}
+
+add_shortcode(
+    "betteruptime_monitor_status",
+    "betteruptime_monitor_status_shortcode"
+);
+
+function betteruptime_monitor_status_enqueue()
+{
+    wp_enqueue_script("jquery");
+}
+
+add_action("wp_enqueue_scripts", "betteruptime_monitor_status_enqueue");
+
+function betteruptime_monitor_status_refresh()
+{
+    ?>
+    <script>
+        jQuery(document).ready(function ($) {
+            function refreshBetterUptimeMonitorStatus() {
+                $.ajax({
+                    url: '<?php echo admin_url("admin-ajax.php"); ?>',
+                    data: {
+                        action: 'betteruptime_monitor_status_refresh'
+                    },
+                    success: function (data) {
+                        $('.betteruptime-monitor-status').replaceWith(data);
+                    }
+                });
+            }
+
+            setInterval(refreshBetterUptimeMonitorStatus, 5 * 60 * 1000);
+        });
+    </script>
+    <?php
+}
+
+add_action("wp_footer", "betteruptime_monitor_status_refresh");
+
+function betteruptime_monitor_status_ajax()
+{
+    echo betteruptime_monitor_status_shortcode();
+    wp_die();
+}
+
+add_action(
+    "wp_ajax_betteruptime_monitor_status_refresh",
+    "betteruptime_monitor_status_ajax"
+);
+add_action(
+    "wp_ajax_nopriv_betteruptime_monitor_status_refresh",
+    "betteruptime_monitor_status_ajax"
+);
---
+++ readme.txt
@@ -0,0 +1,41 @@
+=== Better Stack Monitor Status ===
+Contributors: sanechoice, sohel0415
+Version: 1.0
+Author: SaneChoice Limited
+Author URI: https://www.sanechoice.cloud
+Tags: 1.0
+Requires at least: 6.0
+Tested up to: 6.2
+Stable tag: 1.0
+Requires PHP: 7.4
+License: GPLv3 or later
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+
+Display the status of website monitors using the Better Uptime API.
+
+== Description ==
+If you are using Better Stack for Monitoring, use this plugin to display your monitors directly on your WordPress website.
+
+== Integrate your Better Stack Monitors into WordPress ==
+Better Stack provides website and logs monitoring for websites. This simple plugin will allow you to select your monitors, add your Better Stack API key and create a shortcode to display it anywhere on your website.
+
+== Usages ==
+Add shortcode `[betteruptime_monitor_status]` in any place. That's it :)
+
+== Key Features ==
+* Works with Better Stacks free account.
+* Easily add Better Stack API Key from the Admin Panel.
+* Add individual monitors to display on your website.
+* Creates a shortcode to use on your pages.
+
+== Installation ==
+1. Download the plugin and upload to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Changelog ==
+
+= 1.0 =
+*Release Date - 03 June 2023*
+
+* Show monitors.
+* Setup API and monitors list from admin panel
